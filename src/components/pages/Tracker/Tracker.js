f3f8caa8985139c916d7fa7c50b851d42ee532e9
TP00214906 <gulati.parth98@gmail.com>
2022-02-12 03:47:47 +0530

    Added load all habits API

--- src/components/pages/Tracker/Tracker.js
+++ src/components/pages/Tracker/Tracker.js
@@ -1,31 +1,40 @@
 import { Grid, Button, Typography } from "@mui/material";
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getHabit } from "../../../firebase";
+import { getHabit, auth } from "../../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Tracker = () => {
-
-  const trackerRef = useRef(null)
-  const [habits, setHabits] = useState([])
+  const trackerRef = useRef(null);
+  const [authUser, loading] = useAuthState(auth);
+  const [habits, setHabits] = useState([]);
   let navigate = useNavigate();
 
-  const handleClick = (e) =>{
-    gsap.fromTo(trackerRef.current, {opacity: 1}, {opacity: 0, duration: 1})
-    setTimeout(()=>{
-      navigate('/new-habit')
-    }, 1000)
-  }
+  const handleClick = (e) => {
+    gsap.fromTo(
+      trackerRef.current,
+      { opacity: 1 },
+      { opacity: 0, duration: 1 }
+    );
+    setTimeout(() => {
+      navigate("/new-habit");
+    }, 1000);
+  };
 
-  useEffect(async ()=>{
-    await getHabit().then((response)=>{
-      console.log(response)
-    })
-  }, [])
+  useEffect(async () => {
+    await getHabit(authUser).then((response) => {
+      setHabits([...response]);
+    });
+  }, []);
 
-  useEffect(()=>{
-    gsap.fromTo(trackerRef.current, {opacity: 0}, {opacity: 1, duration: 1})
-  }, [])
+  useEffect(() => {
+    gsap.fromTo(
+      trackerRef.current,
+      { opacity: 0 },
+      { opacity: 1, duration: 1 }
+    );
+  }, []);
 
   return (
     <Grid
@@ -35,11 +44,15 @@ const Tracker = () => {
       ref={trackerRef}
       style={{ height: "80vh" }}
     >
-      <Grid item padding="1.5rem" paddingBottom="2rem">
-        <Button variant="outlined" onClick={handleClick}>
-          <Typography variant="h6" style={{textTransform: 'none'}}>Get Started - Track a habit</Typography>
-        </Button>
-      </Grid>
+      {habits.length === 0 && (
+        <Grid item padding="1.5rem" paddingBottom="2rem">
+          <Button variant="outlined" onClick={handleClick}>
+            <Typography variant="h6" style={{ textTransform: "none" }}>
+              Get Started - Track a habit
+            </Typography>
+          </Button>
+        </Grid>
+      )}
     </Grid>
   );
 };
